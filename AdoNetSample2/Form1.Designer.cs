f0d850551a2fff6c43339e4baceb9148c1e94d6f
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-01-13 19:21:38 +0300

    Добавьте файлы проекта.

---
+++ AdoNetSample2/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AdoNetSample2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbRequest = new System.Windows.Forms.TextBox();
+            this.show = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbRequest
+            // 
+            this.tbRequest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbRequest.Location = new System.Drawing.Point(3, 3);
+            this.tbRequest.Name = "tbRequest";
+            this.tbRequest.Size = new System.Drawing.Size(458, 26);
+            this.tbRequest.TabIndex = 0;
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(476, 0);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 29);
+            this.show.TabIndex = 1;
+            this.show.Text = "Exec";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(564, 253);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbRequest);
+            this.panel1.Controls.Add(this.show);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(564, 38);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 288);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbRequest;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ AdoNetSample2/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AdoNetSample2
+{
+    public partial class Form1 : Form
+    {
+        private SqlDataReader reader;
+        private DataTable table;
+        private SqlConnection conn;
+        private SqlCommand comm;
+
+        string cs = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            cs = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            conn.ConnectionString = cs;
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                try
+                {
+                    SqlCommand comm = new SqlCommand();
+                    comm.CommandText = tbRequest.Text;
+                    comm.Connection = conn;
+
+                    dataGridView1.DataSource = null;
+
+                    conn.Open();
+
+                    table = new DataTable();
+                    reader = comm.ExecuteReader();
+                    int line = 0;
+
+                    do
+                    {
+                        
+                        while (reader.Read())
+                        {
+                            if (line == 0)
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    table.Columns.Add(reader.GetName(i));
+                                }
+                                line++;
+                            }
+                            DataRow row = table.NewRow();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                row[i] = reader[i];
+                            }
+                            table.Rows.Add(row);
+                        }
+                    } while (reader.NextResult());
+                    dataGridView1.DataSource = table;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Probably wrong request syntax");
+                }
+                finally
+                {
+                    // Close the connection
+                    if (conn != null)
+                    {
+                        conn.Close();
+                    }
+                    if (reader != null)
+                    {
+                        reader.Close();
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
